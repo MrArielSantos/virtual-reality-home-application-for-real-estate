fcd4ddd67874f74207959da5f4396a4f0cb2098e
Ariel Santos <104129662+MrArielSantos@users.noreply.github.com>
2022-04-27 14:55:09 +0800

    Add files via upload

---
+++ animation/Anim2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject sphere;
+    // Start is called before the first frame update
+    public void open()
+    {
+        if (sphere != null)
+        {
+            Animator animator = sphere.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool isOpen = animator.GetBool("open");
+
+                animator.SetBool("open", !isOpen);
+            }
+        }
+
+
+    }
+}
---
+++ animation/PanelAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject panel;
+    // Start is called before the first frame update
+    public void open()
+    {
+        if (panel != null)
+        {
+            Animator animator = panel.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool isOpen = animator.GetBool("open");
+
+                animator.SetBool("open", !isOpen);
+            }
+        }
+
+        
+    }
+}
